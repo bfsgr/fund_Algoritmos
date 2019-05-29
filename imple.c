d893312613b999ec57f78506383d740339266c7d
bfsgr <fusiegerb@gmail.com>
2019-05-29 11:04:12 -0300

    asd

--- imple.c
+++ imple.c
@@ -9,19 +9,29 @@ struct livros{
     char editora[50];
 };
 
+int contar(struct livros inv[]){
+  int cont = -1;
+  int i = 0;
+  while(inv[i].autor[0] != '\0'){
+    cont++;
+    i++;
+  }
+  return cont;
+}
+
 void listar(struct livros inv[30]){
     int i;
     printf("ID |Título                        |Autor               |Gênero              |Publicação |Editora                   |Exemplares\n");
     printf("---+------------------------------+--------------------+--------------------+-----------+--------------------------+----------\n");
 
-
-    for(i = 0; i < 6; i++){
-        //ID
+    int tamanho = contar(inv)+1;
+    for(i = 0; i < tamanho; i++){
+      //ID
         //Printa o número de espaços analisando o número de digitos de id
         if(inv[i].id < 10){
             printf("%i  |", inv[i].id);
         } else {
-            printf("%i |", inv[i].id);  
+            printf("%i |", inv[i].id);
         }
 
         //Autor
@@ -39,7 +49,7 @@ void listar(struct livros inv[30]){
 
         //Autor
         //Cria uma int leng que recebe o tamanho do nome do autor
-        leng = strlen(inv[i].autor); 
+        leng = strlen(inv[i].autor);
         //Printa o nome do autor
         printf("%s", inv[i].autor);
         //Usa-se um for para printar o número de espaços até o fim da coluna, neste caso (20 - leng)
@@ -49,9 +59,9 @@ void listar(struct livros inv[30]){
         //Printa a barra separadora de colunas
         printf("|");
 
-        //Gênero 
+        //Gênero
         //Cria uma int leng que recebe o tamanho do gênero
-        leng = strlen(inv[i].genero); 
+        leng = strlen(inv[i].genero);
         //Printa o gênero
         printf("%s", inv[i].genero);
         //Usa-se um for para printar o número de espaços até o fim da coluna, neste caso (20 - leng)
@@ -64,7 +74,7 @@ void listar(struct livros inv[30]){
         //Ano de publicação
         //Printa o ano de publicação
         printf("%i", inv[i].publicacao);
-        //Usa-se um for para printar o número de espaços até o fim da coluna, neste caso 7        
+        //Usa-se um for para printar o número de espaços até o fim da coluna, neste caso 7
         for(j = 0; j < 7 ; j++){
             printf(" ");
         }
@@ -73,7 +83,7 @@ void listar(struct livros inv[30]){
 
         //Editora
         //Cria uma int leng que recebe o tamanho da editora
-        leng = strlen(inv[i].editora); 
+        leng = strlen(inv[i].editora);
         //Printa a editora
         printf("%s", inv[i].editora);
         //Usa-se um for para printar o número de espaços até o fim da coluna, neste caso (26 - leng)
@@ -86,29 +96,74 @@ void listar(struct livros inv[30]){
         //Nº de exemplares
         //Printa o número de exemplares
         printf("%i\n", inv[i].exemplares);
-
-
     }
 }
-void remover(){
+void remover(struct livros *inv, int id){
+  int tamanho = contar(inv)+1;
+  int i,j;
+  for(i = 0; i < tamanho; i++){
+    if(id == inv[i].id){
+      for(j = i; j < tamanho; j++){
+        inv[j].id = inv[j+1].id;
+        inv[j].publicacao = inv[j+1].publicacao;
+        inv[j].exemplares = inv[j+1].exemplares;
+
+        strcpy(inv[j].autor, inv[j+1].autor);
+        strcpy(inv[j].titulo, inv[j+1].titulo);
+        strcpy(inv[j].editora, inv[j+1].editora);
+        strcpy(inv[j].genero, inv[j+1].genero);
+
+      }
+    }
+  }
+
 
 }
-void inserir(){
 
+
+void inserir(struct livros *inv){
+
+  int tamanho = contar(inv)+1;
+  char aux[100];
+  int a;
+  inv[tamanho].id = inv[tamanho-1].id+1;
+
+  printf("Título:");
+  scanf("%s", aux);
+  strcpy(inv[tamanho].titulo, aux);
+
+  printf("Autor:");
+  scanf("%s", aux);
+  strcpy(inv[tamanho].autor, aux);
+
+  printf("Editora: ");
+  scanf("%s", aux);
+  strcpy(inv[tamanho].editora, aux);
+
+  printf("Gênero: ");
+  scanf("%s", aux);
+  strcpy(inv[tamanho].genero, aux);
+
+  printf("Publicação: ");
+  scanf("%i", &inv[tamanho].publicacao);
+
+  printf("Exemplares: ");
+  scanf("%i", &inv[tamanho].exemplares);
 }
+
 void buscar(){
 
 }
 void buscaautor(){
 
 }
-void buscagenero(){
+void mudaEx(){
 
 }
 
 
 int main(){
-    int i;
+    int i, ops = -1, aux;
 
     struct livros inv[30] = {
         {.id = 0, .titulo = "1984", .autor = "George Orwell", .publicacao = 1949, .genero = "Distopia", .editora = "Companhia Das Letras", .exemplares = 10},
@@ -118,13 +173,29 @@ int main(){
         {.id = 4, .titulo = "Divergente", .autor = "Veronica Roth", .publicacao = 2011, .genero = "Aventura", .editora = "Rocco", .exemplares = 10},
         {.id = 5, .titulo = "Codigo da vinci", .autor = "Dan Brown", .publicacao = 2003, .genero = "Romance", .editora = "Arqueiro", .exemplares = 10},
 
-        
-        
     };
-    listar(inv);
+
+    while(ops != 0){
+      printf("------------------------\n0: Sair\n1: Listar\n2: Inserir\n3: Contar\n4: Remover\n\n:::Escolha uma opção: ");
+      scanf("%i", &ops);
+
+      if(ops != 0){
+        if(ops == 1){
+          listar(inv);
+        } else if(ops == 2){
+          inserir(inv);
+        } else if(ops == 3){
+          printf("%i",contar(inv));
+        } else if(ops == 4){
+          printf("Insira o ID que deseja remover: ");
+          scanf("%i", &aux );
+          remover(inv, aux);
+        }
+      }
+
+    }
 
 
 
 
 }
-
